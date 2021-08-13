5177b56826c0220061e106c5fdaf8f4d98548146
Juan Fernando Barrantes Sotelo <barrantesjuan513@gmail.com>
2021-08-12 23:57:53 -0500

    responsive/ menu-searcher 2/js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-menu-left').classList.toggle('show');
+})
+
+document.querySelector('.nav-menu-right').addEventListener('click',()=>{
+    document.querySelector('.lupa').classList.toggle('busca');
+})
